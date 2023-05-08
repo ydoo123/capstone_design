d4c116e0081cb87333ab1fa6cfa2bb844afec162
ydoo123 <ydoo123@gmail.com>
2023-05-08 13:55:56 +0000

    Refactor:

--- myapi/create_db.py
+++ myapi/create_db.py
@@ -1,24 +1,26 @@
 import sqlite3
 
 
-"""create datebase"""
 def create_db():
-    conn = sqlite3.connect('database.db')
+    """create datebase"""
+    conn = sqlite3.connect("database.db")
     return None
 
 
-"""
-Create a table.
-table_name: dest_table
-The columns contain id, cam_id, x, y, theta, w, and time.
-
-Each data type is id: int, cam_id: int, x: float, y:float, theta: float, w:float, time:str.
-"""
 def create_table():
-    conn = sqlite3.connect('database.db')
+    """
+    Create a table.
+    table_name: dest_table
+    The columns contain id, cam_id, x, y, theta, w, and time.
+
+    Each data type is id: int, cam_id: int, x: float, y:float, theta: float, w:float, time:str.
+    """
+    conn = sqlite3.connect("database.db")
     c = conn.cursor()
-    c.execute('''CREATE TABLE dest_table
-                (id integer primary key, cam_id integer, x float, y float, theta float, w float, time text)''')
+    c.execute(
+        """CREATE TABLE dest_table
+                (id integer primary key, cam_id integer, x float, y float, theta float, w float, time text)"""
+    )
     conn.commit()
     conn.close()
     return None
--- myapi/main.py
+++ myapi/main.py
@@ -18,15 +18,25 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-"""
-When you post to /upload_dest, it receives the json data and inserts the json data into the dest_table in database.db.
-"""
 @app.post("/upload_dest")
 def upload_dest(item: Item):
-    conn = sqlite3.connect('database.db')
+    """
+    When you post to /upload_dest, it receives the json data
+    and inserts the json data into the dest_table in database.db.
+    """
+    conn = sqlite3.connect("database.db")
     c = conn.cursor()
-    c.execute("INSERT INTO dest_table (cam_id, x, y, theta, w, time) VALUES (?, ?, ?, ?, ?, ?)",
-            (item.cam_id, item.x, item.y, item.theta, item.w, datetime.datetime.now().strftime(TIME_FORMAT)))
+    c.execute(
+        "INSERT INTO dest_table (cam_id, x, y, theta, w, time) VALUES (?, ?, ?, ?, ?, ?)",  # noqa: E501
+        (
+            item.cam_id,
+            item.x,
+            item.y,
+            item.theta,
+            item.w,
+            datetime.datetime.now().strftime(TIME_FORMAT),
+        ),
+    )
     conn.commit()
     conn.close()
     return {"message": "success"}
@@ -39,7 +49,7 @@ def get_dest():
     c.execute("SELECT * FROM dest_table ORDER BY id DESC LIMIT 1")
     result = c.fetchone()
     conn.close()
-    
+
     dest_dict = dict(zip([x[0] for x in c.description], result))
 
     return dest_dict
